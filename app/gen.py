dd148ea16a694ffd0a26149ed3c201f6b05f11a6
Vladislav ZHereb'ev <111141693+redrepublics@users.noreply.github.com>
2026-06-16 17:19:03 +0300

    #добавлено рандомное время отклика метода в логе, генерация по временным меткам самого лога и записи в нем осуществляется за вчерашний день.

--- app/gen.py
+++ app/gen.py
@@ -1,11 +1,14 @@
-from params_log import *
-import random
 import argparse
+import random
+from datetime import datetime, timedelta
 
+yesterday_pas = datetime.now() - timedelta(days=1)
+current_datetime = yesterday_pas.strftime("%Y-%m-%d %H:%M:%S") + f".{yesterday_pas.microsecond // 1000:03d}"
 # Создаем парсер
 parser = argparse.ArgumentParser(description='Генератор лог-файлов')
 
-file_log = f"date_{datetime.now().strftime('%Y.%m.%d.%H.%M.%S')}.txt"
+yesterday = datetime.now() - timedelta(days=1)
+file_log = f"date_{yesterday.strftime('%Y.%m.%d.%H.%M.%S')}.txt"
 
 parser.add_argument('-n', '--number', type=int, default=30,  help='Количество строк для генерации (по умолчанию: 30)')
 args = parser.parse_args()
@@ -14,9 +17,10 @@ n = args.number
 
 with open(file_log, 'w', encoding='utf-8') as file:
     for i in range(n):
+        num = round(random.random(), 2)
         elements = ['Error 403', 'Ок']
         result_status = random.choices(elements, k=1)
-        error_log_text = f"time: {current_datetime}, GET /api/users/123 HTTP/1.1, время отклика ,{time_stamp} Server status {result_status}"
+        error_log_text = f"time: {current_datetime}, GET /api/users/123 HTTP/1.1, время отклика ,{num} Server status {result_status}"
         # Форматирование строки с инкрементом
         record = f"{error_log_text}\n"
         file.write(record)
--- app/params_log.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f".{datetime.now().microsecond // 1000:03d}"
-dt = datetime.now()
-time_stamp = dt.strftime("%S") + f".{dt.microsecond // 1000:03d}"
