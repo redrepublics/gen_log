849dd78ada988c86af7443c3648ed8368de41688
Vladislav ZHereb'ev <111141693+redrepublics@users.noreply.github.com>
2026-06-16 15:57:16 +0300

    #future 03

--- app/gen.py
+++ app/gen.py
@@ -1,10 +1,17 @@
 # from datetime import datetime
 from params_log import *
+import random
 
+
+file_log = f"date_{datetime.now().strftime('%H-%M-%S-%F')}.txt"
 N = int(input("Введите количество строк  "))  # заданное количество записей
+
 
 with open(file_log, 'w', encoding='utf-8') as file:
     for i in range(N):
+        elements = ['Error 403', 'Ок']
+        result_status = random.choices(elements, k=1)
+        error_log_text = f"time: {current_datetime}, GET /api/users/123 HTTP/1.1, время отклика ,{time_stamp} Server status {result_status}"
         # Форматирование строки с инкрементом
         record = f"{error_log_text}\n"
         file.write(record)
--- app/params_log.py
+++ app/params_log.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-pass_log_text = '# 192.30.253.112 - - [02.01.2018:20:59:51 +0100] "GET /dolorem/dicta.csv HTTP/1.0" 200 5039 "https://example1.com/" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_9) AppleWebKit/5351 (KHTML, как у Геккона) Chrome/14.0.850.0 Safari/5351'
-error_log_text = '# 192.30.253.112  Error 404'
-current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-file_log = f"date_{current_datetime}.txt"
+import time
+
+current_datetime = datetime.now().strftime("%YYYY-%MM-%D %HH:%S:%M:%MS")
+time_stamp = datetime.now().strftime("%S:%MS")
+
+# YYYY-MM-DDThh:mm:ss:ms <имя метода> <время отклика> <OK|ERROR>
